28e3464df95a0cd1ca90a108dacb8614d764fbce
M-Mongy <mohamedmongy96@gmail.com>
2026-05-07 11:25:35 +0300

    feat(auth): add signup and login endpoints

--- src/app.js
+++ src/app.js
@@ -3,12 +3,16 @@ require("dotenv").config();
 const express = require('express')
 const app = express()
 
+app.use(express.json());
+
 const connectDB = require("./config/db");
 connectDB();
 
 const setupSwagger = require("./config/swagger");
 setupSwagger(app);
 
-app.use(express.json());
+//auth routes
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
